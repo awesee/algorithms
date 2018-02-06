bfb6e09541c1a2b0e8b6b1d26e3bb0b0587d4694
Openset <openset.wang@gmail.com>
2018-02-06 17:56:08 +0800

    Add: Rabbit

---
+++ go/Rabbit.go
@@ -0,0 +1,29 @@
+/*
+
+题目：古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，
+	小兔子长到第三个月后每个月又生一对兔子，
+	假如兔子都不死，问n个月后的兔子总数为多少？
+
+程序分析：兔子的规律为数列1,1,2,3,5,8,13,21...
+
+*/
+
+package main
+
+func main() {
+
+	println(Rabbit(3)) // 2
+
+	println(Rabbit(10)) // 55
+
+	println(Rabbit(20)) // 6765
+}
+
+func Rabbit(n uint) uint {
+
+	if n <= 2 {
+		return 1
+	}
+
+	return Rabbit(n-1) + Rabbit(n-2)
+}
